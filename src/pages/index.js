d0ea9f238a02cddb089c48c7c44d1e344c8454b2
ptmartins <pedro@pedromartins.com.pt>
2023-10-02 19:31:51 +0100

    Revert code.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,30 +7,6 @@ import styles from '@/styles/Home.module.css';
 
 export default function Home({recipes}) {
 
-  const[data, setData] = useState([]);
-
-  /**
-   * Shuffle recipes
-   * @param {*} arr 
-   */
-  const shuffleRecipes = arr => {
-    const shuffledArray = arr.slice();
-    for (let i = shuffledArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-    }
-
-    setData(shuffledArray.slice(0, 2));
-    localStorage.getItem('recipes') === undefined ? localStorage.setItem('recipes', data) : null;
-  }
-
-  const refreshRecipes = () => {
-    localStorage.getItem('recipes') !== undefined ? localStorage.removeItem('recipes') : null;
-    shuffleRecipes(recipes);
-  }
-
-  useEffect(() => shuffleRecipes(recipes), [])
-
   return (
     <>
       <Head>
@@ -41,11 +17,11 @@ export default function Home({recipes}) {
       </Head>
       <main className=""> 
         <div className={styles.actions}>
-          <button className="btn btn--cta" onClick={() => refreshRecipes()}>Refresh</button>
+          <button className="btn btn--cta">Refresh</button>
           <button className="btn btn--cta">Show ingredients</button>
         </div>
         <div className={styles.recipes}>
-          {data.map((recipe, index) => <RecipeTile key={index} data={recipe.fields} />)}
+          {recipes.map((recipe, index) => <RecipeTile key={index} data={recipe.fields} />)}
         </div>
       </main>
     </>
